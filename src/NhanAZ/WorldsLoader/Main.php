743a969836ee69102dafa9f5fa4aecff628f8807
taylordevs <nguyenhieucoder47@gmail.com>
2023-07-05 08:41:57 +0700

    Added World Auto Converter

--- src/NhanAZ/WorldsLoader/Main.php
+++ src/NhanAZ/WorldsLoader/Main.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace NhanAZ\WorldsLoader;
 
+use GlobalLogger;
 use pocketmine\plugin\PluginBase;
+use pocketmine\world\format\io\exception\UnsupportedWorldFormatException;
+use pocketmine\world\format\io\FormatConverter;
+use pocketmine\world\format\io\WorldProviderManager;
+use pocketmine\world\format\io\WorldProviderManagerEntry;
+use pocketmine\world\format\io\WritableWorldProviderManagerEntry;
 
 class Main extends PluginBase {
 
@@ -12,10 +18,33 @@ class Main extends PluginBase {
 		$worlds = array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]);
 		$lodedWorld = [];
 		foreach ($worlds as $worldName) {
-			if ($this->getServer()->getWorldManager()->loadWorld($worldName)) {
-				$lodedWorld[] = $worldName;
-			}
+            try {
+                if ($this->getServer()->getWorldManager()->loadWorld($worldName)) {
+                    $lodedWorld[] = $worldName;
+                }
+            } catch (UnsupportedWorldFormatException) {
+                $providerManager = new WorldProviderManager();
+                $writableFormats = array_filter(
+                    $providerManager->getAvailableProviders(),
+                    fn(WorldProviderManagerEntry $class) => $class instanceof WritableWorldProviderManagerEntry
+                );
+                $worldPath = $this->getServer()->getDataPath() . "worlds" . DIRECTORY_SEPARATOR . $worldName;
+                $backupPath = $this->getDataFolder() . $worldName;
+                $toFormat = $this->getServer()->getConfigGroup()->getProperty("level-settings.default-format", "leveldb");
+                $oldProviderClasses = $providerManager->getMatchingProviders($worldPath);
+                if(count($oldProviderClasses) === 0){
+                    $this->getLogger()->warning("{$worldName} world format unknown!");
+                }
+                if (count($oldProviderClasses) > 1) {
+                    $this->getLogger()->warning("Ambiguous world format: matched " . count($oldProviderClasses) . " (" . implode(array_keys($oldProviderClasses)) . ")");
+                }
+                $oldProviderClass = array_shift($oldProviderClasses);
+                $oldProvider = $oldProviderClass->fromPath($worldPath, new \PrefixedLogger(\GlobalLogger::get(), "Old World Provider"));
+                $converter = new FormatConverter($oldProvider, $writableFormats[$toFormat], $backupPath, GlobalLogger::get());
+                $converter->execute();
+                $lodedWorld[] = $worldName;
+            }
 		}
-		$this->getLogger()->info("Worlds loaded successfully: [" . implode(", ", $lodedWorld) . "]");
+        $this->getLogger()->info("Worlds loaded successfully: [" . implode(", ", $lodedWorld) . "]");
 	}
 }
